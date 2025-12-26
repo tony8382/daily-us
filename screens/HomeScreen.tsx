8919f3bb6eccee352b76bb815af9fa8b019e2e6a
Li-Yu, Yang <tony830802@gmail.com>
2025-12-26 15:49:25 +0800

    feat: implement scroll to top

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { View, FlatList, ActivityIndicator } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import { HomeHeader } from '../components/home/HomeHeader';
 import { MoodStatusCard } from '../components/home/MoodStatusCard';
 import { MemoryCard } from '../components/home/MemoryCard';
@@ -9,6 +10,21 @@ import { t } from '../i18n/t';
 
 export default function HomeScreen() {
     const { profile, mood, feed, loading } = useDailyUsData();
+    const ref = useRef<FlatList>(null);
+    const navigation = useNavigation();
+
+    useEffect(() => {
+        // Manually handle tab press to ensure scroll-to-top works
+        // @ts-ignore - tabPress is valid for tab navigators, ignoring inference issue
+        const unsubscribe = navigation.addListener('tabPress', (e) => {
+            if (navigation.isFocused()) {
+                // If already focused, scroll to top
+                ref.current?.scrollToOffset({ offset: 0, animated: true });
+            }
+        });
+
+        return unsubscribe;
+    }, [navigation]);
 
     if (loading) {
         return (
@@ -23,6 +39,7 @@ export default function HomeScreen() {
             <HomeHeader profile={profile} />
 
             <FlatList
+                ref={ref}
                 data={feed}
                 keyExtractor={(item) => item.id}
                 renderItem={({ item }) => <MemoryCard item={item} />}
